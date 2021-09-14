16324cc1694f42e547413ed3052abbed4625550e
jwtavare <jwtavare@gmail.com>
2021-09-14 16:34:00 -0400

    First build

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,3 +1,13 @@
+import requests
+import sys
+
+modulename = 'requests'
+if modulename not in sys.modules:
+    print 'You have not imported the {} module'.format(modulename)
+
+response = requests.get("http://api.open-notify.org/astros.json")
+print(response)
+
 alien_0 = {'color': 'goldish', 'points': 5}
 print(alien_0)
 alien_0['x_position'] = 0
