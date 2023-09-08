f4aee57138fff39bd51d4d8feeed18f524e95f9d
Jason Tavares <jwtavare@gmail.com>
2023-09-08 16:18:31 -0400

    Latest

---
+++ pythonProject1/main.py
@@ -0,0 +1,17 @@
+
+todos = []
+
+while True:
+   user_action = input("Type add or show:")
+   
+   match user_action:
+         case "add":
+            todo = input("Enter a todo: ")
+   print(todos)
+
+
+
+
+
+Jay
+
