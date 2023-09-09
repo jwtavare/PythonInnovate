9760e371134dc40c3b1b2c2988900811cac7d381
Jason Tavares <jwtavare@gmail.com>
2023-09-09 12:12:57 -0400

    Class 2

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -2,17 +2,18 @@
 todos = []
 
 while True:
-   user_action = input("Type add or show:")
+   user_action = input("Type add, show, or exit:")
    
    match user_action:
          case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
          case "show":
-            print(todos)
+            for item in todos:
+               print(item)
          case "exit":
             break
-
+         
 print("Goodbye!")
 
 
